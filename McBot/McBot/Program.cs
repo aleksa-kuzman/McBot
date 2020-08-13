85072b640d2f60f81b6e6108c55f5577c03aa9c8
Aleksa <aleksa.kuzman.996@gmail.com>
2020-08-13 22:17:43 +0200

    ADD: connect to web socket discord API

--- McBot/McBot/Program.cs
+++ McBot/McBot/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MC_Server_Starter
@@ -30,13 +32,27 @@ namespace MC_Server_Starter
         private static async Task RunAsync()
         {
             var gateway = await GetWebSocketGateway();
+            await SendHello(gateway.Url);
+        }
+
+        private static async Task SendHello(string uri)
+        {
+            try
+            {
+                ClientWebSocket socketClient = new ClientWebSocket();
+                await socketClient.ConnectAsync(new Uri(uri), CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
         private static async Task<GatewayResource> GetWebSocketGateway()
         {
             using (HttpClient Client = new HttpClient())
             {
-                Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bot", "NzQxMzUyOTczMTAzMjY3OTgy.Xy2Uwg.TIno-a3-lTSKMIpjRwVvORcbKA0");
+                Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bot", "NzQxMzUyOTczMTAzMjY3OTgy.Xy2Uwg.OSMLFuKsMX399XwkW6AiA4KXURw");
                 Client.DefaultRequestHeaders.Add("User-Agent", "myTestAp | this is a");
 
                 HttpResponseMessage message = await Client.GetAsync("https://discord.com/api/gateway/bot");
