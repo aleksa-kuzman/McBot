b5d5503ce64976ac8652a39a170292b597e4eb7d
Aleksa <aleksa.kuzman.996@gmail.com>
2020-08-13 22:33:16 +0200

    WIP

--- McBot/McBot/Program.cs
+++ McBot/McBot/Program.cs
@@ -1,9 +1,11 @@
 ﻿using MC_Server_Starter.Gateway;
+using McBot;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Net.WebSockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -33,7 +35,24 @@ namespace MC_Server_Starter
         {
             var gateway = await GetWebSocketGateway();
             await SendHello(gateway.Url);
+            //await SendMessage();
         }
+
+       /* private static async Task SendMessage()
+        {
+            using (HttpClient Client = new HttpClient())
+            {
+                Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bot", "NzQxMzUyOTczMTAzMjY3OTgy.Xy2Uwg.OSMLFuKsMX399XwkW6AiA4KXURw");
+                Client.DefaultRequestHeaders.Add("User-Agent", "myTestAp | this is a");
+
+                var json = new Message("Hey man hi, server up", false);
+                HttpContent content = new StringContent(JsonConvert.SerializeObject(json), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage message = await Client.PostAsync("https://discord.com/api/channels/741382227249856544/messages?payload_json", content);
+                var somethingElse = JsonConvert.DeserializeObject<GatewayResource>(await message.Content.ReadAsStringAsync());
+                Console.WriteLine(somethingElse);
+            }
+        }*/
 
         private static async Task SendHello(string uri)
         {
