a37b1c6387e1472359846825a64dc470482d6c33
Aleksa <aleksa.kuzman.996@gmail.com>
2020-08-14 17:56:41 +0200

    REFACTOR: More phasing out things from main program

---
+++ McBot/McBot/Contracts/IDiscordWebSocketApi.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace McBot.Contracts
+{
+    public interface IDiscordWebSocketApi
+    {
+        Task<bool> ConnectToSocketApi(string uri);
+
+        Task IdentifyToSocket(string uri, ClientWebSocket clientWebSocket);
+    }
+}
---
+++ McBot/McBot/Core/DiscordWebSocketApi.cs
@@ -0,0 +1,85 @@
+﻿using McBot.Contracts;
+using McBot.Gateway.Payloads;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace McBot.Core
+{
+    public class DiscordWebSocketApi : IDiscordWebSocketApi
+    {
+        private readonly ClientWebSocket _clientWebSocket;
+
+        public DiscordWebSocketApi(ClientWebSocket clientWebSocket)
+        {
+            _clientWebSocket = clientWebSocket;
+        }
+
+        public async Task<bool> ConnectToSocketApi(string uri)
+        {
+            await _clientWebSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+            var payload = await RecievePayload();
+            if (payload.op == 10)
+                return true;
+            else
+                return false;
+        }
+
+        public async Task IdentifyToSocket(string uri, ClientWebSocket clientWebSocket)
+        {
+            try
+            {
+                GatewayPayload payload = new GatewayPayload();
+                payload.op = 2;
+                var dataPayload = new IdentifyDataPayload();
+                dataPayload.Token = "NzQxMzUyOTczMTAzMjY3OTgy.Xy2Uwg.OSMLFuKsMX399XwkW6AiA4KXURw";
+                dataPayload.Properties = new IdentifyDataPayloadProperties("linux", "my_library", "MyClient");
+
+                payload.d = dataPayload;
+
+                var jsonSettings = new JsonSerializerSettings();
+                jsonSettings.NullValueHandling = NullValueHandling.Ignore;
+                var json = JsonConvert.SerializeObject(payload, Formatting.Indented, jsonSettings);
+                var bytes = Encoding.UTF8.GetBytes(json);
+                await _clientWebSocket.SendAsync(bytes, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                var recievedPayload = await RecievePayload();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private async Task<GatewayPayload> RecievePayload()
+        {
+            var buffer = new ArraySegment<byte>(new byte[2048]);
+            WebSocketReceiveResult result;
+            using (var memmoryStream = new MemoryStream())
+            {
+                do
+                {
+                    result = await _clientWebSocket.ReceiveAsync(buffer, CancellationToken.None);
+                    memmoryStream.Write(buffer.Array, buffer.Offset, result.Count);
+                }
+                while (!result.EndOfMessage);
+
+                GatewayPayload payload = null;
+                if (result.MessageType != WebSocketMessageType.Close)
+                {
+                    memmoryStream.Seek(0, SeekOrigin.Begin);
+                    using (var reader = new StreamReader(memmoryStream, Encoding.UTF8))
+                    {
+                        payload = JsonConvert.DeserializeObject<GatewayPayload>(await reader.ReadToEndAsync());
+                        Console.WriteLine(await reader.ReadToEndAsync());
+                    }
+                }
+                return payload;
+            }
+        }
+    }
+}
